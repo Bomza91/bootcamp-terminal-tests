be5b89cf2d0ab9051bd64cc6bae11058c3d61c77
Bomza91 <bmasentile@gmail.com>
2020-08-18 10:30:29 +0200

    changes

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -10,6 +10,10 @@ describe("The countAllFromTown function", function() {
       assert.equal(4, countAllFromTown ("CL 123,CL 321,CY 133,CL 123,CL 123","CL"));
     });
 
+    it("should count Stellenbosch registrations", function() {
+      assert.equal(6, countAllFromTown ("CL 123,CL 321,CL 123,CL 321,CY 133,CL 123,CL 123","CL"));
+    });
+
 
   });
 
--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -8,4 +8,9 @@ describe ("The countAllPaarl function", function(){
     
     });
 
+    it("should return all CJ registrations", function() {
+      assert.equal(5, countAllPaarl ("CJ 123, CJ 124, CJ 163, CJ 321, CY 133, CJ 246","CJ"));
+    
+    });
+
 });
